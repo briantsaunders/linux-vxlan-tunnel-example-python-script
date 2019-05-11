6b2850ad7fd93b327b2586750ab6cd5dc9034172
Brian Saunders <brian.t.saunders@gmail.com>
2019-05-11 08:47:31 -0500

    modify IPDB vxlan create sequence due to bug

--- linux_vxlan_tunnel_example.py
+++ linux_vxlan_tunnel_example.py
@@ -18,45 +18,60 @@ logger.addHandler(console)
 def add_tunnel(args):
     """
     """
-    with IPDB() as ipdb:
-        try:
-            ipdb.interfaces[args.physical_interface]
-        except KeyError:
-            logger.error(f"{args.physical_interface} interface does not exist")
-            sys.exit()
-        with ipdb.interfaces[args.physical_interface] as i:
-            i.up()
-        with ipdb.create(
-            kind="vxlan",
-            ifname=f"vxlan{args.vni}",
-            vxlan_id=args.vni,
-            vxlan_learning=False,
-            vxlan_local=args.local_vtep,
-            vxlan_group=args.remote_vtep,
-        ) as i:
-            i.up()
-            logger.info(f"vxlan interface vxlan{args.vni} created")
-        with ipdb.create(kind="bridge", ifname=f"br{args.vni}") as i:
-            i.add_port(ipdb.interfaces[f"vxlan{args.vni}"].index)
-            i.add_port(ipdb.interfaces[args.physical_interface].index)
-            logger.info(f"bridge interface br{args.vni} created")
-            logger.info(
-                f"vxlan{args.vni} and {args.physical_interface} added to br{args.vni}"
-            )
-        with ipdb.interfaces[f"br{args.vni}"] as i:
-            i.up()
-
+    ipdb = IPDB()
+    try:
+        ipdb.interfaces[args.physical_interface]
+    except KeyError:
+        logger.error(f"{args.physical_interface} interface does not exist")
+        sys.exit()
+    ipdb.interfaces[args.physical_interface].up()
+    ipdb.interfaces[args.physical_interface].commit()
+    ipdb.create(
+        kind="vxlan",
+        ifname=f"vxlan{args.vni}",
+        vxlan_id=args.vni,
+        vxlan_learning=1,
+        vxlan_local=args.local_vtep,
+        vxlan_group=args.remote_vtep
+    )
+    ipdb.interfaces[f"vxlan{args.vni}"].commit()
+    ipdb.interfaces[f"vxlan{args.vni}"].up()
+    ipdb.interfaces[f"vxlan{args.vni}"].commit()
+    logger.info(f"vxlan interface vxlan{args.vni} created")
+    ipdb.create(kind="bridge", ifname=f"br{args.vni}")
+    ipdb.interfaces[f"br{args.vni}"].add_port(ipdb.interfaces[f"vxlan{args.vni}"].index)
+    ipdb.interfaces[f"br{args.vni}"].add_port(ipdb.interfaces[args.physical_interface].index)
+    ipdb.interfaces[f"br{args.vni}"].up()
+    ipdb.interfaces[f"br{args.vni}"].commit()
+    logger.info(f"bridge interface br{args.vni} created")
+    logger.info(
+        f"vxlan{args.vni} and {args.physical_interface} added to br{args.vni}"
+    )
 
 def delete_tunnel(args):
     """
     """
-    with IPDB() as ipdb:
-        with ipdb.interfaces[f"vxlan{args.vni}"] as i:
-            i.remove()
-        logger.info(f"vxlan{args.vni} deleted")
-        with ipdb.interfaces[f"br{args.vni}"] as i:
-            i.remove()
-        logger.info(f"br{args.vni} deleted")
+    ipdb = IPDB()
+    try:
+        ipdb.interfaces[args.physical_interface]
+    except KeyError:
+        logger.error(f"{args.physical_interface} interface does not exist")
+        sys.exit()
+    try:
+        ipdb.interfaces[f"vxlan{args.vni}"]
+    except KeyError:
+        logger.error(f"vxlan{args.vni} interface does not exist")
+        sys.exit()
+    try:
+        ipdb.interfaces[f"br{args.vni}"]
+    except KeyError:
+        logger.error(f"br{args.vni} interface does not exist")
+        sys.exit()
+    ipdb.interfaces[f"vxlan{args.vni}"].remove()
+    logger.info(f"vxlan{args.vni} deleted")
+    ipdb.interfaces[f"br{args.vni}"].remove()
+    logger.info(f"br{args.vni} deleted")
+    ipdb.commit()
 
 
 def main(args):
